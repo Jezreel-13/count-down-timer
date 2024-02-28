30a1b5a63d998644206cc98f6023571d33da59bc
jezreel <jaymoneymbaonu@gmail.com>
2024-02-29 01:58:33 +0400

    fixed date error

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const months = [
   let tempMonth = tempDate.getMonth();
   let tempDay = tempDate.getDate();
   // months are ZERO index based;
-  const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+  const futureDate = new Date(tempYear, tempMonth, tempDay + 20, 7, 30, 0);
   // console.log(futureDate);
   // let futureDate = new Date(2020, 3, 24, 11, 30, 0);
   
